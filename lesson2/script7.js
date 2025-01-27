492d5efc149d213a67dc42f7c02b47804acbaa0d
Gloriaxp17 <135527907+Gloriaxp17@users.noreply.github.com>
2025-01-27 22:47:46 +0200

    Add files via upload

---
+++ lesson2/script7.js
@@ -0,0 +1,9 @@
+`use strict`
+
+
+//7
+let month = Math.floor(Math.random() * 12 + 1);
+let week = Math.floor(Math.random() * 7);
+let random = month + week;
+document.write(` random: ${random}`);
+
